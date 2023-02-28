a2cf0144645b7687a121277abaed9d261da6b0ad
4118 Марков П.И <markov-pt@mail.ru>
2023-02-28 11:42:10 +0300

    add some assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import styles from "./App.module.scss"
+import numberOne from "./assets/numbersToCount/1.jpg"
+import numberTwo from "./assets/numbersToCount/2.jpg"
+import numberThree from "./assets/numbersToCount/3.jpg"
+import zero from "./assets/numbersToCount/0.jpg"
 
 function App() {
 	return (
-		<div className={styles.app}>
-			<h1 className="underline">Hello VK!!!</h1>
+		<div>
+			<h1 className="underline">
+				<span>Hello VK!!!</span>
+				<div className="flex items-center">
+					<img alt="number one" src={numberOne} />
+					<img alt="number one" src={numberTwo} />
+					<img alt="number one" src={numberThree} />
+					<img alt="number one" src={zero} />
+					<img alt="number one" src={numberThree} />
+				</div>
+			</h1>
 		</div>
 	)
 }
